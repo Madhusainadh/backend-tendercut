e99266ad47c72844714a3de3f1dcf9a403be6532
jstgrowup <deysubham999@gmail.com>
2022-12-25 21:41:58 +0530

    Adding some security Features

--- Routes/Address.route.js
+++ Routes/Address.route.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const AddressModel = require("../Schemas/Address");
 const Address = Router();
+const bcrypt = require("bcryptjs");
 Address.post("/find", async (req, res) => {
   const { number } = req.body;
 
@@ -14,11 +15,36 @@ Address.post("/find", async (req, res) => {
   }
 });
 Address.post("/create", async (req, res) => {
-  try {
-    const add = await AddressModel.create(req.body);
-    res.send(add);
-  } catch (error) {
-    res.status(401).send(error.message);
+  const {
+    name,
+    email,
+    password,
+    address,
+    flatNumber,
+    landmark,
+    pincode,
+    number,
+  } = req.body;
+  let existingAddress = await AddressModel.findOne(email, number);
+  if (existingAddress) {
+    res.status(401).send({ message: "User Already exists please Login" });
+  } else {
+    try {
+      // const salt = await bcrypt.genSalt(10);
+      // const securedPass = await bcrypt.hash(password, salt);
+      const add = await AddressModel.create({
+        name: name,
+        email: email,
+        password: password,
+        address: address,
+        flatNumber: flatNumber,
+        landmark: landmark,
+        pincode: pincode,
+      });
+      res.send(add);
+    } catch (error) {
+      res.status(401).send(error.message);
+    }
   }
 });
 Address.post("/create/multiple", async (req, res) => {
